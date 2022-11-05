484011a899871859a77f1b74d2dbc7662ba4436b
Vimal Bhoutekar <vimalbhoutekar@gmail.com>
2022-11-05 21:49:33 +0530

    Second Change

---
+++ 01_sum_of_numbers.c
@@ -0,0 +1,13 @@
+// Two Number Sum Program 
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    int sum;
+    printf("Enter The Value A and B is : \n");
+    scanf("%d %d",&a,&b);
+    sum= a+b;
+    printf("Sum is %d",sum);
+
+}
